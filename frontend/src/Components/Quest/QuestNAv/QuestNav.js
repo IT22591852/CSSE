d900b75975cae19a445de53aa2433b2fd80e4dbd
IT22591852 <127759332+IT22591852@users.noreply.github.com>
2024-10-14 13:20:19 +0530

    userLogin

--- frontend/src/Components/Quest/QuestNAv/QuestNav.js
+++ frontend/src/Components/Quest/QuestNAv/QuestNav.js
@@ -7,24 +7,6 @@ function Nav() {
   
   return (
     
-    // <div>
-    //    <ul className="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
-    //   {/* <img  src={logo} alt='logo'></img> */}
-    //     <li className="home-li">
-    //         <Link to="/mainhome" className="activehome-a">
-    //         <h1>Home</h1></Link>
-    //     </li>
-        
-    //     <li className="home-li">
-    //     <Link to="/addnote" className="activehome-a">
-    //         <h1>Add Note</h1></Link>
-    //     </li>
-    //     <li className="home-li">
-    //     <Link to="/notedetails" className="activehome-a">
-    //         <h1>Note details</h1></Link>
-    //     </li>
-    //     </ul>
-    // </div>
     <div>
     <div className="nav_con_user">
       <div>
